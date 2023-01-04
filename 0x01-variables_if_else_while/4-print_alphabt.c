8890c2d6015974e2be1ca40fb570e5e19df7a1cd
michaelmas01 <michaelmasabayomi@gmail.com>
2023-01-03 19:43:14 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -16,10 +16,9 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-	if (letter != 'e' && letter != 'q')
-		putchar(letter);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
-	
 	putchar('\n');
 
 	return (0);

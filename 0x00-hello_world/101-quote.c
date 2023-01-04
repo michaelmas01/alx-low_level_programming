34db53a287cb35d664df086c36033e793f387b88
michaelmas01 <michaelmasabayomi@gmail.com>
2023-01-03 22:02:11 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 /*
- *  * File: 101-quote.c
- *   * Auth: Brennan D Baraban
- *    */
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
 
 #include <unistd.h>
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   * followed by a new line, to standard error.
- *    *
- *     * Return: Always 1.
- *      */
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 
 int main(void)
 {

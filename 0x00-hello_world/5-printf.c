41dc1a9ccdd493b31e1ff2007704d550f9dffb46
michaelmas01 <michaelmasabayomi@gmail.com>
2023-01-03 22:16:22 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,10 @@
-/*
- * File: 5-printf.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-/**
- * main - Prints "with proper grammar, but the outcome is a piece of art," followed by a new line.
- *
- * Return: Always 0.
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
